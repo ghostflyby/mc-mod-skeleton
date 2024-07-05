1d332db4cd4ffa6acd21f46ca3780d3654d63782
ghostflyby <ghostflyby+git@outlook.com>
2024-07-05 19:01:50 +0800

    access root project property in common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 architectury {
-    common(property("enabled_platforms").toString().split(","))
+    common(rootProject.property("enabled_platforms").toString().split(","))
 }
 
 loom {
@@ -12,7 +12,7 @@ loom {
 dependencies {
     // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
     // Do NOT use other classes from fabric loader
-    modImplementation("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
     // Remove the next line if you don't want to depend on the API
     // modApi("dev.architectury:architectury:${property("architectury_version")}")
 }
