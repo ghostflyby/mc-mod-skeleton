4e03e00ba7b9dce96b6d1350c8a5d3fd8d7294c2
ghostflyby <ghostflyby+git@outlook.com>
2024-07-11 14:46:44 +0800

    use only subprojects instead of allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 plugins {
     java
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.architectury.plugin)
     alias(libs.plugins.architectury.loom) apply false
     alias(libs.plugins.spotless)
@@ -13,64 +13,50 @@ architectury {
     minecraft = libs.versions.minecraft.get()
 }
 
-spotless {
-    kotlin {
-        ktlint()
-    }
-}
-
 subprojects {
-    apply(
-        plugin =
-            rootProject.libs.plugins.architectury.loom
-                .get()
-                .pluginId,
-    )
-
-    val loom = project.extensions.getByName<LoomGradleExtensionAPI>("loom")
-
-    dependencies {
-        with(rootProject) {
-            "minecraft"(libs.minecraft)
-
-            // The following line declares the yarn mappings you may select this one as well.
-            @Suppress("UnstableApiUsage")
-            "mappings"(
-                loom.layered {
-                    mappings(libs.yarn.mapping)
-                    mappings(libs.yarn.patch)
-                },
-            )
-        }
-    }
-}
-
-allprojects {
     fun applyPlugin(plugin: Provider<PluginDependency>) {
         apply(plugin = plugin.get().pluginId)
     }
     apply(plugin = "java")
-    with(rootProject) {
-        applyPlugin(libs.plugins.kotlin.asProvider())
-        applyPlugin(libs.plugins.kotlin.serialization)
-        applyPlugin(libs.plugins.architectury.plugin)
-    }
+    val libs = rootProject.libs
+    applyPlugin(libs.plugins.kotlin.asProvider())
+    applyPlugin(libs.plugins.kotlin.serialization)
+    applyPlugin(libs.plugins.architectury.plugin)
+    applyPlugin(libs.plugins.architectury.loom)
+    applyPlugin(libs.plugins.spotless)
 
     base.archivesName.set(rootProject.property("archives_base_name").toString())
     version = rootProject.property("mod_version").toString()
     group = rootProject.property("group").toString()
 
+    val loom = project.extensions.getByName<LoomGradleExtensionAPI>("loom")
+
     dependencies {
+        "minecraft"(libs.minecraft)
+
+        // The following line declares the yarn mappings you may select this one as well.
+        @Suppress("UnstableApiUsage")
+        "mappings"(
+            loom.layered {
+                mappings(libs.yarn.mapping)
+                mappings(libs.yarn.patch)
+            },
+        )
+
+        api(libs.kotlinx.serialization)
+        api(libs.kotlinx.coroutines)
         api(kotlin("stdlib"))
         api(kotlin("serialization"))
         api(kotlin("reflect"))
-        with(rootProject) {
-            api(libs.kotlinx.serialization)
-            api(libs.kotlinx.coroutines)
-        }
     }
 
     java {
         withSourcesJar()
+    }
+
+    spotless {
+        kotlin {
+            ktlint()
+        }
     }
 }
