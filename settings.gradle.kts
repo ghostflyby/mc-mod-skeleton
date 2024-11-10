d40bfe064ec4d38d1da64a2c843b633344794da0
ghostflyby <ghostflyby+git@outlook.com>
2024-11-10 11:25:37 +0800

    Revert "declare repositories in settings.gradle.kts"
    This reverts commit 3d5b554e864f0965e28aa1c9b7461b384f999c2a.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,29 +12,11 @@ dependencyResolutionManagement {
   @Suppress("UnstableApiUsage")
   repositories {
     mavenCentral()
-    maven {
-      name = "ParchmentMC"
-      setUrl("https://maven.parchmentmc.org")
-    }
-    maven {
-      setUrl("https://maven.neoforged.net/releases/")
-    }
-    maven { setUrl("https://maven.architectury.dev/") }
-    maven { setUrl("https://maven.fabricmc.net/") }
-    // KFF
-    maven {
-      name = "Kotlin for Forge"
-      setUrl("https://thedarkcolour.github.io/KotlinForForge/")
-    }
-    // forgified fabric api
-    maven {
-      setUrl("https://maven.su5ed.dev/releases")
-    }
   }
 }
 
-include(":common")
-include(":neoforge")
-include(":fabric")
+include("common")
+include("neoforge")
+include("fabric")
 
 rootProject.name = "examplemod"
